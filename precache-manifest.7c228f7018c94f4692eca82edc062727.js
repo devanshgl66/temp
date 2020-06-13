dcdb73a653bd0993d08c8f6a529c6fd7ed496808
devanshgl66 <devanshgl55@gmail.com>
2020-06-13 23:51:11 +0530

    Updates

---
+++ precache-manifest.7c228f7018c94f4692eca82edc062727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ecadb040478447bae4642ae5de0339",
+    "url": "/temp/index.html"
+  },
+  {
+    "revision": "e0485ca6fe572e28ed92",
+    "url": "/temp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6a47aea6471001ad92b",
+    "url": "/temp/static/js/2.704ff7d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/temp/static/js/2.704ff7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0485ca6fe572e28ed92",
+    "url": "/temp/static/js/main.5d3bb0c8.chunk.js"
+  },
+  {
+    "revision": "fb49e98014126e4318d2",
+    "url": "/temp/static/js/runtime-main.9f67c32f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/temp/static/media/logo.5d5d9eef.svg"
+  }
+]);
